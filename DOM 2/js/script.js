d860b7490b6acdb99816b2643523a9b44977c854
Brenno Vilas Boas <brennovilasboas91@gmail.com>
2026-06-10 16:48:58 -0300

    DOM

---
+++ DOM 2/js/script.js
@@ -0,0 +1,7 @@
+var itensQuery = document.querySelectorAll('#lista2 li')
+
+
+var itensQuery2 = document.querySelectorAll('#lista .item')
+
+
+var itensQuery3 = document.querySelector('#lista')
---
+++ DOM/js/script.js
@@ -0,0 +1,22 @@
+
+var titulo = document.getElementsByTagName('h1')[0]
+
+var lis = document.getElementsByTagName('li')
+
+var paragrafo = document.getElementById('paragrafo')
+
+var itensDaLista = document.getElementsByClassName('item')
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
