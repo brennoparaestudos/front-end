23dec59e7fb10f525ec6453250093ec72b44f6a3
Brenno Vilas Boas <brennovilasboas91@gmail.com>
2026-06-11 12:35:48 -0300

    dom 3

---
+++ DOM 3/js/script.js
@@ -0,0 +1,10 @@
+var title = document.querySelector('#title')
+
+// innerHTML
+title.innerHTML = 'Testando o texto alterado'
+
+// textContent -> mais utilizado, recomendado e performático
+
+var subtitile = document.querySelector(".subtitle")
+
+subtitile.textContent = 'Testando o textContent'
