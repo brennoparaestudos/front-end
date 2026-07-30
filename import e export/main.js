66948dbe93af9ca5b55d6a6ff91b8af6d25a2b57
Brenno Vilas Boas <brennovilasboas91@gmail.com>
2026-07-29 21:15:49 -0300

    Export e Import em JS

---
+++ import e export/main.js
@@ -0,0 +1,12 @@
+//import Hero from './superHero.class.js'
+import { printHeroName as heroName, printHeroPower as HeroPower} from './superHero.class.js'
+
+
+const hero = new Hero('Homem-Aranha', 'Teias')
+
+HeroName(hero)
+HeroPower(hero)
+
+//console.log(hero.name);
+//console.log(hero.power);
+
