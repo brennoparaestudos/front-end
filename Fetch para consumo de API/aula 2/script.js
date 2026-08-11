67815990d4cd871e458b68d9411564d89c858d80
Brenno Vilas Boas <brennovilasboas91@gmail.com>
2026-08-11 20:43:43 -0300

    aula 2 fetch API

---
+++ Fetch para consumo de API/aula 2/script.js
@@ -0,0 +1,22 @@
+
+const p_temp = document.getElementById('p_temp')
+const p_nivel = document.getElementById('p_nivel')
+const p_press = document.getElementById('p_press')
+
+const obterDados = () =>{
+    const endpoint = 'http://localhost:8080'
+
+    fetch(endpoint)
+
+    .then(res=>res.json())
+
+    .then(dados=>{
+        console.log(dados)
+
+        p_temp.innerHTML = 'Temperatura: ' + dados.temperatura
+        p_nivel.innerHTML = 'Nivel: '+ dados.nivel
+        p_press.innerHTML = "Pressão: " + dados.pressao
+    })
+}
+
+let intervalo = setInterval(obterDados, 3000)
