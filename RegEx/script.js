10637f821203629436a675146672b84117f5f690
Brenno Vilas Boas <brennovilasboas91@gmail.com>
2026-08-20 17:35:59 -0300

    RegEx

---
+++ RegEx/script.js
@@ -0,0 +1,6 @@
+let text = 'este é um texto'
+let regex = /texto/g
+
+let results = regex.exec(text)
+
+console.log(results)
--- mercado-pago.js
+++
@@ -1,6 +0,0 @@
-const m = 'mercado pago'
-const d = 'devedor'
-console.log(m, d)
-const p = 'pagamento'
-
-console.log(m, p)
